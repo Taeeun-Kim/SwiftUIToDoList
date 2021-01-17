379b92aa6239db66fa92dfba1ecf41846099bcff
Taeeun Kim <taeeun.kim@outlook.com>
2021-01-17 12:50:32 +0100

    Add "add funtion" and "delete function" with CoreData

--- SwiftUIToDoList/ContentView.swift
+++ SwiftUIToDoList/ContentView.swift
@@ -9,6 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @Environment(\.managedObjectContext) var context
+    
+    @FetchRequest(fetchRequest: ToDoListItem.getAllToDoListItems())
+    var items: FetchedResults<ToDoListItem>
+    
+    
     @State var text: String = ""
     
     var body: some View {
@@ -20,6 +26,21 @@ struct ContentView: View {
                         
                         Button(action: {
                             
+                            if !text.isEmpty{
+                                let newItem = ToDoListItem(context: context)
+                                newItem.name = text
+                                newItem.createdAt = Date()
+                                
+                                do {
+                                    try context.save()
+                                }
+                                catch{
+                                    print(error)
+                                }
+                                
+                                text = ""
+                            }
+                            
                         }, label: {
                             Text("Save")
                         })
@@ -27,7 +48,25 @@ struct ContentView: View {
                 }
                 
                 Section{
-                    Text("abc")
+                    ForEach(items){ toDoListItem in
+                        VStack(alignment: .leading){
+                            Text(toDoListItem.name!)
+                                .font(.headline)
+                            Text("\(toDoListItem.createdAt!)")
+                        }
+                    }.onDelete(perform: { indexSet in
+                        guard let index = indexSet.first else{
+                            return
+                        }
+                        let itemToDelete = items[index]
+                        context.delete(itemToDelete)
+                        do{
+                            try context.save()
+                        }
+                        catch{
+                            print(error)
+                        }
+                    })
                 }
             }
             .navigationTitle("To Do List")
